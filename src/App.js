bcf2b81ec6cab06a47b9056e181774bee0586598
高源 <2338090565@qq.com>
2025-02-17 17:25:40 +0800

    fliter is finish

--- src/App.js
+++ src/App.js
@@ -3,11 +3,33 @@ import Form from "./components/Form";
 import Todo from "./components/Todo";
 import { useState } from "react";
 import { nanoid } from "nanoid";
+import { use } from "react";
 
 
 export default function App(props) {
 
+
+  const [filter, setFilter] = useState("All");
+
   const [tasks, setTasks] = useState(props.tasks);
+
+  const FILTER_MAP = {
+    All: () => true,
+    Active: (task) => !task.completed,
+    Completed: (task) => task.completed,
+  };
+
+  const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
+
 
 
   function addTask(name) {
@@ -20,6 +42,19 @@ export default function App(props) {
     setTasks(remainingTasks);
   }
 
+
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // Copy the task and update its name
+        return { ...task, name: newName };
+      }
+      // Return the original task if it's not the edited task
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
 
 
   function toggleTaskCompleted(id) {
@@ -36,15 +71,20 @@ export default function App(props) {
   }
 
 
-  const taskList = tasks?.map((task) => (
-    <Todo
-      id={task.id}
-      name={task.name}
-      completed={task.completed}
-      key={task.id}
-      toggleTaskCompleted={toggleTaskCompleted}
-      deleteTask={deleteTask}
-    />));    //映射todo
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    ));
+
 
 
   const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
@@ -56,9 +96,7 @@ export default function App(props) {
       <h1>TodoMatic</h1>
       <Form onSubmit={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton filterName="All" />
-        <FilterButton filterName="Active" />
-        <FilterButton filterName="Completed" />
+        {filterList}
       </div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
